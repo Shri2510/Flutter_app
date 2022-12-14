512dc4a05080ae4af4f8480f0af58b172610b6dc
Shri <magdumsourabh85@gmail.com>
2022-12-14 12:55:26 -0800

    Day_2 Completed homepage,scaffold added

---
+++ lib/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Text("Welcome to $days of Flutter!"),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
